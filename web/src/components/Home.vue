5c2bf43ef9e3d3e02c95c7f564d6ef582d92c9b6
Anton Maisak <am@rus-bit.com>
2018-05-17 14:56:58 +0300

    mobile fixes

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <main class="h-100 clr-bg-indigo-500 clr-pal-def-white p-t-5">
+    <main class="h-100 clr-bg-indigo-500 clr-pal-def-white p-t-5-lg p-t-5-md p-4-sm p-4-smx">
       <article class="cont">
-      <h1  class="txt-sz-3 txt-c txt-uppercase txt-100">{{title}}</h1>
-      <div class="txt-c p-t-3 p-b-3">
-        <img :alt="title" src="/static/logo.svg" width=180 class="animation-rubberBand" />
+      <h1  class="txt-sz-3 txt-sz-2-sm txt-sz-2-smx txt-c txt-uppercase txt-100">{{title}}</h1>
+      <div class="txt-c p-t-2 p-b-3">
+        <img :alt="title" src="/static/logo.svg" width="180" height="180" class="animation-rubberBand" />
       </div>
       <p class="txt-j m-b-3 txt-sz-1-2">{{description}}</p>
 
       <div class="rw m-b-3">
-          <div class="cl w-12 txt-c">
-            <a :href="systemv" class="btn clr-pal-def-white m-r-1 txt-sz-1-4 txt-uppercase"><i class="mi-book"></i>systemv</a>
-            <a :href="systemd" class="btn clr-pal-def-white txt-sz-1-4 txt-uppercase">systemd</a>
+          <div class="cl w-6 txt-c-xs txt-c-sm txt-r-lg txt-l-md">
+            <a :href="systemv" class="btn clr-pal-def-white m-r-1 txt-sz-1-4 txt-sz-0-9-sm txt-sz-0-9-smx txt-uppercase">systemv</a>
+          </div>
+          <div class="cl w-6 txt-c-xs txt-c-sm txt-l-lg txt-l-md">
+            <a :href="systemd" class="btn clr-pal-def-white txt-sz-1-4 txt-sz-0-9-sm txt-sz-0-9-smx txt-uppercase">systemd</a>
           </div>
       </div>
 
@@ -19,8 +21,8 @@
         <div class="cl w-12 txt-c opacity-0-8">
           <a :href="github" class=" clr-vf-pal-def-white m-r-1"><i class="mi-github txt-sz-1-3"></i></a>
           <a :href="vk" class="clr-pal-def-white m-r-1"><i class="mi-vk txt-sz-1-3"></i></a>
-          <a :href="`mailto:${mail}`" class="clr-pal-def-white m-r-2"><i class="mi-envelope-out txt-sz-1-7"></i></a>
-          <a :href="mirror" class="clr-pal-def-white ">Официальное зеркало LFS</a>
+          <a :href="`mailto:${mail}`" class="clr-pal-def-white m-r-2-lg m-r-2-md"><i class="mi-envelope-out txt-sz-1-7 txt-sz-1-4-smx txt-sz-1-4-sm"></i></a>
+          <a :href="mirror" class="clr-pal-def-white  d-block-sm d-block-smx p-t-1-smx p-t-1-sm txt-sz-0-8-smx txt-sz-0-8-sm">Официальное зеркало LFS</a>
         </div>
       </div>
     </article>
@@ -41,7 +43,7 @@ export default {
       mirror: 'https://linuxfromscratch.org.ru/',
       vk: 'https://vk.com/linuxfromscratch_ru',
       systemv: '/systemv/LFS-BOOK.html',
-      systemd: '/systemd/LFS-BOOK.html',
+      systemd: '/systemd/LFS-SYSD-BOOK.html',
       mail: 'am@rus-bit.com'
     }
   }
